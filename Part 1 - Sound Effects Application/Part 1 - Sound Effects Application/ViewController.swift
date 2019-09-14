1a3e55e9128a420a2624212660f9a39758dce83c
James Yakas <YAKASJ02@myunitec.ac.nz>
2019-09-14 14:46:38 +1200

    Added sounds for Animals -> Land
    Added tags (0-2) for land animal buttons.
    
    Imported Sounds folder into Xcode.
    
    Set the class for the View controller regarding the land animal sounds to "ViewController" in the Assisant Editor.
    
    Drag a sound button from land animals into the viewController class (code).
    Set the button type to UIButton and called the function soundPressed.
    
    Dragged the remaining 2 sound buttons onto the function.
    
    Importanted AVFoundation.
    
    Created a soundList array containing all the sounds, Soundn
    Created a soundName string
    
    Checked if the button tag is less than the array size and if the button tag is greater tthan or equal to 0
    Set soundName to the correct sound in the array based of the button tag.
    
    Played the sound.

--- Part 1 - Sound Effects Application/Part 1 - Sound Effects Application/ViewController.swift
+++ Part 1 - Sound Effects Application/Part 1 - Sound Effects Application/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func soundPressed(_ sender: UIButton) {
+        // List of sound file names
+        let soundList = ["Sound0", "Sound1", "Sound2"]
+        var soundName = ""
+        // Checks to ensure the tag number is within the bounds of the array
+        if sender.tag < soundList.count && sender.tag >= 0 {
+            soundName = soundList[sender.tag]
+        }
+        
+        
+        // Gets the sound resource
+        if let soundResource = Bundle.main.url(forResource: soundName, withExtension: "wav") {
+            do {
+                // Think of this as putting CD in a CD player
+                audioPlayer = try AVAudioPlayer(contentsOf: soundResource)
+                
+                // Plays the sound file
+                audioPlayer?.play()
+            } catch  {
+                // Catches any errors that might occur while putting the CD in the CD player
+                print(error)
+            }
+        }
+    }
 }
 
