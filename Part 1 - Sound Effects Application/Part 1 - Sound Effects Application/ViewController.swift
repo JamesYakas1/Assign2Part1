d47b2f8e438dd350c91227387f686a56837822cb
James Yakas <YAKASJ02@myunitec.ac.nz>
2019-09-15 13:51:17 +1200

    Added the remaing sounds & function to pause when switch tab
    Addeed the remaing sounds for the app using the same logic in the previous commits.
    
    Connected all sounds to be able to be toggled on/off.
    
    Added the function for the sound to pause when switching to another tab, so that two sounds, one from each tab, cannot be simultaneously played.

--- Part 1 - Sound Effects Application/Part 1 - Sound Effects Application/ViewController.swift
+++ Part 1 - Sound Effects Application/Part 1 - Sound Effects Application/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { //, AVAudioPlayerDelegate
 
     var audioPlayer : AVAudioPlayer?
     var labelName:String = ""
@@ -19,12 +19,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Loaded")
+        //Reset the toogle label to "On/Off" everytime a category is selected
+        //labelName = "On/Off"
+//        self.audioPlayer?.delegate = self
     }
+    
+//    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+//
+//        print("The song ended")
+//    }
 
+    //Function to pause sound when changing tabs
+    override func viewWillDisappear(_ animated: Bool) {
+        if let viewControllers = self.navigationController?.viewControllers {
+            if viewControllers.count > 1 && viewControllers[viewControllers.count-2] == self {
+                print("New view controller was pushed")
+            }
+            else {
+                print("View controller was popped")
+                audioPlayer?.pause()
+                
+                //Attempt to fix bug
+                let button = self.view.viewWithTag(27) as! UIButton
+                button.setTitle("\(labelName) is paused.", for: .normal)
+                if (button.titleLabel!.text == "is paused."){
+                    button.setTitle("\(labelName) is paused.", for: .normal)
+                }
+            }
+            
+        }
+    }
     
     @IBAction func soundPressed(_ sender: UIButton) {
         // List of sound file names
-        let soundList = ["Sound0", "Sound1", "Sound2", "Sound3", "Sound4", "Sound5", "Sound6","Sound7","Sound8"]
+        let soundList = ["Sound0", "Sound1", "Sound2", "Sound3", "Sound4", "Sound5", "Sound6","Sound7","Sound8","Sound9","Sound10","Sound11", "Sound12", "Sound13", "Sound14", "Sound15", "Sound16", "Sound17", "Sound18", "Sound19", "Sound20", "Sound21", "Sound22", "Sound23", "Sound24", "Sound25", "Sound26"]
         var soundName = ""
         // Checks to ensure the tag number is within the bounds of the array
         if sender.tag < soundList.count && sender.tag >= 0 {
@@ -41,19 +70,24 @@ class ViewController: UIViewController {
                 audioPlayer?.pause()
                 sender.setTitle("\(labelName) is paused.", for: .normal)
                 
-                //audioPlayer?.stop()
+                //Attempt to fix bug
+                let button = self.view.viewWithTag(27) as! UIButton
+                if (button.titleLabel!.text == "is paused."){
+                    button.setTitle("\(labelName) is paused.", for: .normal)
+                }
             }else {//audioPlayer!.isPlaying == false{
                 print("Resumed")
                 audioPlayer?.play()
-                sender.setTitle("\(labelName) is playing.", for: .normal) //= "\(labelName) is playing."
+                sender.setTitle("\(labelName) is playing.", for: .normal)
             }
             }
         }
         
+        //If not tooggle On/Off
         if sender.tag != 27{
             print(sender.titleLabel!.text!)
             labelName = sender.titleLabel!.text!
-            var button = self.view.viewWithTag(27) as! UIButton
+            let button = self.view.viewWithTag(27) as! UIButton
             button.setTitle("\(labelName) is playing.", for: .normal)
         // Gets the sound resource
         if let soundResource = Bundle.main.url(forResource: soundName, withExtension: "wav") {
@@ -63,20 +97,6 @@ class ViewController: UIViewController {
                 
                 // Plays the sound file
                 audioPlayer?.play()
-                print("Paused2")
-                //Toggle pause/resume
-//                if sender.tag == 27{
-//                    if audioPlayer?.isPlaying == true{
-//                        print("Paused")
-//                        //audioPlayer?.isPlaying = false
-//                        audioPlayer?.pause()
-//                        //audioPlayer?.stop()
-//                    }else {//audioPlayer!.isPlaying == false{
-//                        print("Resumed")
-//                        audioPlayer?.play()
-//                    }
-//
-//                }
             } catch  {
                 // Catches any errors that might occur while putting the CD in the CD player
                 print(error)
